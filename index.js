4a223fd15ea58ce14d5c297d19cf387530bf5e6b
Sepehr Laal <sepehr.laal@gmail.com>
2021-11-23 21:40:13 -0800

    added a guardrail option to the CLI

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const program = require("commander");
 const package = require("./package.json");
 
 require("./lib/vendor/evb");
+require("./lib/commands/guardrail");
 
 program.version(package.version, "-v, --vers", "output the current version");
 program.parse(process.argv);
