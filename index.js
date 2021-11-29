1d9a3f39413e0ebb424a4e83ba92bdaa4db8eb4e
Sepehr Laal <sepehr.laal@gmail.com>
2021-11-29 15:08:26 -0800

    fixed typo in version alias

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const package = require("./package.json");
 
 require("./lib/commands/guardrail");
 
-program.version(package.version, "-v, --vers", "output the current version");
+program.version(package.version, "-v, --version", "output the current version");
 program.parse(process.argv);
 
 if (process.argv.length < 3) {
