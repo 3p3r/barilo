77be72f2d9d38902b5cf8234099451d11a40854b
Sepehr Laal <sepehr.laal@gmail.com>
2021-12-02 12:56:39 -0800

    looks nicer...

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 process.env.AWS_SDK_LOAD_CONFIG = 1;
 
 const { program } = require("commander");
-const package = require("./package.json");
+const { version } = require("./package.json");
 
 require("./lib/commands/guardrail");
 
-program.version(package.version, "-v, --version", "output the current version");
+program.version(version, "-v, --version", "output the current version");
 program.parse(process.argv);
 
 if (process.argv.length < 3) {
